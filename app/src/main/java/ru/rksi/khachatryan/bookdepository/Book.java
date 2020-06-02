41908a805e09f689ad12a4b50d1b08c5ba287af5
AlenX23 <alenx6123@gmail.com>
2020-06-02 14:38:56 +0300

    SP

---
+++ app/src/main/java/ru/rksi/khachatryan/bookdepository/Book.java
@@ -0,0 +1,72 @@
+package ru.rksi.koleukhov.bookdepository;
+
+import android.util.Log;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Book
+{
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mReaded;
+
+    public Book()
+    {
+        this(UUID.randomUUID());
+    }
+
+    public Book(UUID id)
+    {
+        mId = id;
+        mDate = new Date();
+    }
+
+    public void setTime(Date time)
+    {
+        mDate.setHours(time.getHours());
+        mDate.setMinutes(time.getMinutes());
+    }
+
+    private void setTime(long time)
+    {
+        mDate.setTime(time);
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date)
+    {
+        //long time = mDate.getTime();
+        mDate = date;
+        //this.setTime(time);
+    }
+
+    public boolean isReaded() {
+        return mReaded;
+    }
+
+    public void setReaded(boolean readed) {
+        mReaded = readed;
+    }
+
+    public String getPhotoFilename()
+    {
+        return "IMG_" + getId().toString() + ".jpg";
+    }
+}
